c0ed0bbb8627cc38f4a18e9fe30536e55dbd7873
RustamHuseynov02 <rustem.huseynov.2016@gmail.com>
2024-05-06 18:52:12 +0400

    Algorithmic codes were written in jwt service

---
+++ jwt/jwt-token/src/main/java/com/example/jwttoken/service/JwtService.java
@@ -0,0 +1,10 @@
+package com.example.jwttoken.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JwtService {
+
+	String generateToken(String username);
+
+	Boolean validateToken(String token, UserDetails userDetails);
+}
---
+++ jwt/jwt-token/src/main/java/com/example/jwttoken/service/impl/JwtServiceImpl.java
@@ -0,0 +1,74 @@
+package com.example.jwttoken.service.impl;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import com.example.jwttoken.service.JwtService;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+
+	@Value("${jwt.key}")
+	private String SECRET;
+
+	@Override
+	public String generateToken(String username) {
+		Map<String, Object> claims = new HashMap<>();
+		claims.put("rustam", "huseynov");
+		return createToken(claims, username);
+	}
+	
+	@Override
+	public Boolean validateToken(String token,UserDetails userDetails) {
+		String username = extractUser(token);
+		Date expirationDate = extractExpiration(token);
+		return userDetails.getUsername().equals(username) && !expirationDate.before(new Date());
+	}
+	
+	private Date extractExpiration(String token) {
+		Claims claims = Jwts
+				.parserBuilder()
+				.setSigningKey(getSignKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+		  return claims.getExpiration();
+	}
+	
+	private String extractUser(String token) {
+		Claims claims = Jwts
+				.parserBuilder()
+				.setSigningKey(getSignKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+		  return claims.getSubject();
+	}
+
+	private String createToken(Map<String, Object> claims, String username) {
+		return Jwts.builder()
+				.setClaims(claims)
+				.setSubject(username)
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() * 1000 * 60 * 2))
+				.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+	}
+
+	private Key getSignKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+
+}
